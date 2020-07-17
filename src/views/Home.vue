fe7834ba6aa2ab1efa995edd1dd04041f7c5a963
Goldwin Japar <goldwin.japar@gdn-commerce.com>
2020-07-17 16:02:46 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="profile">
+      <img alt="goldwin_japar" src="@/assets/avatar.jpg" class="profile__avatar-img">
+      <div class="profile__introduction">
+        <h3 class="profile__name">goldwin japar</h3>
+        <p class="profile__about">
+          software development engineer
+          <a href="https://www.blibli.com/" target="_blank">@blibli.com</a>
+        </p>
+        <div class="profile__links">
+          <a href="https://github.com/golz">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
+          <a href="https://www.linkedin.com/in/goldwin-japar/">
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+          </a>
+          <a href="https://www.facebook.com/goldwin.japar/">
+            <font-awesome-icon :icon="['fab', 'facebook']" />
+          </a>
+          <a href="mailto:goldwin.japar@gmail.com">
+            <font-awesome-icon icon="envelope" />
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script src="./js/home.js"></script>
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+  @import "~@/assets/scss/utils/variable";
+
+  .home {
+    height: 600px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    user-select: none;
   }
-}
-</script>
+  .profile {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &__avatar-img {
+      height: 150px;
+      width: auto;
+      border-radius: 50%;
+      margin-right: 32px;
+    }
+    &__introduction {
+      a {
+        text-decoration: none;
+        color: $blue;
+      }
+    }
+    &__name {
+      font-size: $font-size-large;
+    }
+    &__about {
+      color: $gray;
+    }
+    &__links {
+      a {
+        color: $blue-light-2;
+        font-size: $font-size;
+        &:not(:last-child) {
+          margin-right: 8px;
+        }
+      }
+    }
+  }
+</style>
